2d5fbb771ecab155409a7bfc667bf248628f32b0
Zhong Jianxin <azuwis@gmail.com>
2026-04-23 19:42:07 +0800

    Use fence 0.1.50

--- default.nix
+++ default.nix
@@ -9,7 +9,16 @@ let
         "claude-code"
       ];
   };
-  fence-agent = pkgs.callPackage ./nix/fence-agent.nix { };
+  fence-agent = pkgs.callPackage ./nix/fence-agent.nix {
+    # For new config `allowLocalOutboundPorts`
+    fence = pkgs.fence.overrideAttrs (old: {
+      version = "0.1.50";
+      src = old.src.override {
+        hash = "sha256-avWQkOWRf1qby/wUSieDiusX5M1Vg00CrvclOZFvp5s=";
+      };
+      vendorHash = "sha256-JIkEe+wscowc1IT8gtm5C4ZnChsOhy5wTy7R//DLFTU=";
+    });
+  };
   fence-claude = pkgs.callPackage ./nix/fence-claude.nix { inherit fence-agent; };
   fence-pi = pkgs.callPackage ./nix/fence-pi.nix { inherit fence-agent; };
 in
